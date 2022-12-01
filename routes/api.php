6c73a928e18fb0c7fd9a975aa02b4c2145af6ca7
hotdog05481 <hotdog05481@gmail.com>
2022-12-01 09:15:47 +0800

    123

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Route::apiResource('posts', 'App\Http\Controllers\Api\PostController');
+Route::post('item', 'App\Http\Controllers\ItemController@store');
